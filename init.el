9d79dc222bd7406b2b294ba12d07f4cf5cb06c4f
Akihiro Uchida <akihiro.uchida@world-scape.net>
2015-03-17 16:27:22 +0900

    markdownをEmacsでプレビューするための設定

--- init.el
+++ init.el
@@ -919,5 +919,26 @@ This is particularly useful under Mac OSX, where GUI apps are not started from a
                              (powerline-fill face2 (powerline-width rhs))
                              (powerline-render rhs)))))))
 
+;; markdown-preview
+(require 'w3m)
+(require 'markdown-mode)
+
+(defun w3m-browse-url-other-window (url &optional newwin)
+  (let ((w3m-pop-up-windows t))
+    (if (one-window-p) (split-window))
+    (other-window 1)
+    (w3m-browse-url url newwin)))
+
+(defun markdown-render-w3m (n)
+  (interactive "p")
+  (message (buffer-file-name))
+  (call-process "/usr/local/bin/grip" nil nil nil
+                "--gfm" "--export"
+                (buffer-file-name)
+                "/tmp/grip.html")
+  (w3m-browse-url-other-window "file://tmp/grip.html")
+  )
+(define-key markdown-mode-map "\C-c \C-c" 'markdown-render-w3m)
+
 ;; スタートアップ非表示
 (setq inhibit-startup-message t)
